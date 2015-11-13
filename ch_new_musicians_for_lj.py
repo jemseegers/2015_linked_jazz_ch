40c1c417bfaba5eba11e6d4668476b4b5a96f60e
jemseegers <jem.seegers@gmail.com>
2015-11-13 13:54:40 -0500

    getting going!

--- ch_new_musicians_for_lj.py
+++ ch_new_musicians_for_lj.py
@@ -1,3 +1,4 @@
+import re
 from rdflib import Graph
 
 ch = Graph()
@@ -17,4 +18,30 @@ for s,p,o in ch:
 			if o!=a:
 				if o not in newmusicians:
 					newmusicians.append(o)
-					print(newmusicians)
+					#print(newmusicians)
+
+with open ("newbies_to_lj.json", "w") as g:
+	g.write(json.dumps(newmusicians, indent=4))
+
+
+
+#convert list into string for regex. Add space between each object to assist compile
+# 					chstring = ' '.join(newmusicians)
+
+# #print (newmusicians)
+
+# #compile pattern to find only names (The names appear to all start with capital letters,
+# #and I don't see any other capital letters in the CH data set of name URIs. So this pulls out
+# #a string from the first capital letter until a space, which you now have between each URI)
+
+# 					x = re.compile('([A-X][^\s]+)')
+
+# #search method
+# 					m = x.findall(chstring)
+
+# 					for person in m:
+# 					    print (person)
+
+
+#you may have to refine the regular expression parameters, but that was my idea on how to isolate
+#these names.
